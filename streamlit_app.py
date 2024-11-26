ff4d3704a497ba3c13546df38d183eed9dba50fa
aaloni1 <aaloni@ssct.edu.ph>
2024-11-26 15:13:14 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ blog_posts = [
             ("Birthdate", "October 27, 2005"),
             ("Birthplace", "Quiapo, Manila"),
         ],
-       "image": "https://scontent.fcgy2-4.fna.fbcdn.net/v/t39.30808-6/447232820_485701057364609_6882034262964220786_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHjriIDTRJC_PcvJAfgIouOFYiAgsWwUSoViICCxbBRKheimFO4y58FaaCHAq9X9ZazI6GTSJfowGXCwAWihCGQ&_nc_ohc=BKxVkhXz20EQ7kNvgHPdImd&_nc_zt=23&_nc_ht=scontent.fcgy2-4.fna&_nc_gid=Ai8oJezuBIWDV3zOmo6nf18&oh=00_AYC8O_UH06Ya04GiX9_yRbtBH9FcVgi15JusViQXFAD3pQ&oe=674B3112"  # Replace with your image path or URL
+        "image": "https://scontent.fcgy2-4.fna.fbcdn.net/v/t39.30808-6/447232820_485701057364609_6882034262964220786_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=a5f93a&_nc_eui2=AeHjriIDTRJC_PcvJAfgIouOFYiAgsWwUSoViICCxbBRKheimFO4y58FaaCHAq9X9ZazI6GTSJfowGXCwAWihCGQ&_nc_ohc=BKxVkhXz20EQ7kNvgHPdImd&_nc_zt=23&_nc_ht=scontent.fcgy2-4.fna&_nc_gid=Ai8oJezuBIWDV3zOmo6nf18&oh=00_AYC8O_UH06Ya04GiX9_yRbtBH9FcVgi15JusViQXFAD3pQ&oe=674B3112"  # Sample image URL
     }
 ]
 
@@ -25,8 +25,8 @@ for post in blog_posts:
     for item in post["content"]:
         st.write(f"**{item[0]}:** {item[1]}")
     
-    # Display the image
+    # Display the image with a 1:1 aspect ratio, without a caption
     if "image" in post:
-        st.image(post["image"], caption="This is an image of Adam Rey G. Aloni", use_column_width=True)
+        st.image(post["image"], width=150, height=150)  # 1:1 aspect ratio without caption
     
     st.markdown("---")
